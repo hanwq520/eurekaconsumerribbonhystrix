c82ebcf57f57f4c9e7326a5800980bd37cfffd5d
hanwq520 <1146542352@qq.com>
2019-05-20 15:11:47 +0800

    Initial commit

---
+++ src/main/java/com/hanwq/TestController.java
@@ -0,0 +1,44 @@
+package com.hanwq;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+/**
+ * Description: ddeurekaclient
+ * Created by hanwq on 2019/5/16 16:15
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    ConsumerService consumerService;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return consumerService.consumer();
+    }
+
+    @Service
+    class ConsumerService {
+
+        @Autowired
+        RestTemplate restTemplate;
+
+        @HystrixCommand(fallbackMethod = "fallback")
+        public String consumer() {
+            return restTemplate.getForObject("http://eureka-client/dc", String.class);
+        }
+
+        public String fallback() {
+            return "fallbck";
+        }
+
+    }
+
+
+}
